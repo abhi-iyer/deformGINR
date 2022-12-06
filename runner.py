81e0082a5205c3f97488b0bb862046bc912eb532
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2022-12-05 19:51:21 -0800

    updated

--- runner.py
+++ runner.py
@@ -7,6 +7,32 @@ from data import *
 from ginr_experiment import *
 from ginr_configs import CONFIGS
 
+# regular_bunny_10_fourier = GINR_Experiment("regular_bunny_10_fourier", CONFIGS["regular_bunny_10_fourier"])
+# regular_bunny_10_fourier.train(num_epochs=5000, show_plot=False)
 
-basic_bunny = GINR_Experiment("basic_bunny", CONFIGS["bunny_config"])
-basic_bunny.train(num_epochs=5000, show_plot=False)
+# regular_bunny_20_fourier = GINR_Experiment("regular_bunny_20_fourier", CONFIGS["regular_bunny_20_fourier"])
+# regular_bunny_20_fourier.train(num_epochs=5000, show_plot=False)
+
+# regular_bunny_30_fourier = GINR_Experiment("regular_bunny_30_fourier", CONFIGS["regular_bunny_30_fourier"])
+# regular_bunny_30_fourier.train(num_epochs=5000, show_plot=False)
+
+# regular_bunny_40_fourier = GINR_Experiment("regular_bunny_40_fourier", CONFIGS["regular_bunny_40_fourier"])
+# regular_bunny_40_fourier.train(num_epochs=5000, show_plot=False)
+
+# regular_bunny_50_fourier = GINR_Experiment("regular_bunny_50_fourier", CONFIGS["regular_bunny_50_fourier"])
+# regular_bunny_50_fourier.train(num_epochs=5000, show_plot=False)
+
+# regular_bunny_60_fourier = GINR_Experiment("regular_bunny_60_fourier", CONFIGS["regular_bunny_60_fourier"])
+# regular_bunny_60_fourier.train(num_epochs=5000, show_plot=False)
+
+# regular_bunny_70_fourier = GINR_Experiment("regular_bunny_70_fourier", CONFIGS["regular_bunny_70_fourier"])
+# regular_bunny_70_fourier.train(num_epochs=5000, show_plot=False)
+
+# regular_bunny_80_fourier = GINR_Experiment("regular_bunny_80_fourier", CONFIGS["regular_bunny_80_fourier"])
+# regular_bunny_80_fourier.train(num_epochs=5000, show_plot=False)
+
+# regular_bunny_90_fourier = GINR_Experiment("regular_bunny_90_fourier", CONFIGS["regular_bunny_90_fourier"])
+# regular_bunny_90_fourier.train(num_epochs=5000, show_plot=False)
+
+regular_bunny_100_fourier = GINR_Experiment("regular_bunny_100_fourier", CONFIGS["regular_bunny_100_fourier"])
+regular_bunny_100_fourier.train(num_epochs=5000, show_plot=False)
